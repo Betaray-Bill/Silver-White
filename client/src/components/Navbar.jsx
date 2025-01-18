346e221421974dd5e15bee8c03c5b7544988efaf
Abhinandhan S <abhi.varadan@gmail.com>
2025-01-19 02:15:24 +0530

    again nav changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <>
       <nav className="flex justify-around items-center py-4 px-8 w-[50rem]">
-        <div className="text-xl font-bold">RentRide</div>
-        <div className="flex gap-4 bg-gray-200 rounded-xl text-black">
+        <div className="text-2xl font-bold">SilverWhite</div>
+        <div className="flex bg-gray-200 rounded-xl text-black">
           <Link to="/" className="hover:bg-gray-300 hover:rounded-xl cursor-pointer p-4 transition-all duration-300 ease-in-out">
             Buy Cars
           </Link>
